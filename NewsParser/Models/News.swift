bcf046bfa35dc3459742fe8bd9b7fbca0386b565
Dmitri Breshchuk <breschuk1@gmail.com>
2021-03-21 21:39:28 +0300

    Fix news properties and add functionality for convert data into dictionary for Firebase

--- NewsParser/Models/News.swift
+++ NewsParser/Models/News.swift
@@ -14,35 +14,39 @@ struct News {
     let title : String
     var titleImageURL : String? = nil
     var textUnderTitleImage : String? = nil
+    let briefExplanation : String
     let author: [String]
     let language: String
     let date: String
     let text: [String]
     var imagesURL : [String]? = nil
     let tags: [String]
-    var ref: DatabaseReference?
     
-    init(newsURL: String, title: String,titleImageURL: String,textUnderTitleImage: String, author: [String], language: String, date: String, text: [String], imagesURL: [String], tags: [String]) {
-        self.init(newsURL: newsURL, title: title, author: author, language: language, date: date, text: text, tags: tags)
+    init(newsURL: String, title: String, briefExplanation: String ,titleImageURL: String, textUnderTitleImage: String, author: [String], language: String, date: String, text: [String], imagesURL: [String], tags: [String]) {
+        self.init(newsURL: newsURL, title: title,briefExplanation: briefExplanation, author: author, language: language, date: date, text: text, tags: tags)
         self.titleImageURL = titleImageURL
         self.textUnderTitleImage = textUnderTitleImage
         self.imagesURL = imagesURL
     }
     
-    init(newsURL: String, title: String,titleImageURL: String,textUnderTitleImage: String, author: [String], language: String, date: String, text: [String], tags: [String]) {
-        self.init(newsURL: newsURL, title: title, author: author, language: language, date: date, text: text, tags: tags)
+    init(newsURL: String, title: String, briefExplanation: String , titleImageURL: String, textUnderTitleImage: String, author: [String], language: String, date: String, text: [String], tags: [String]) {
+        self.init(newsURL: newsURL, title: title,briefExplanation: briefExplanation, author: author, language: language, date: date, text: text, tags: tags)
         self.textUnderTitleImage = textUnderTitleImage
         self.titleImageURL = titleImageURL
     }
     
-    init(newsURL: String, title: String, author: [String], language: String, date: String, text: [String], tags: [String]) {
+    init(newsURL: String, title: String,briefExplanation: String, author: [String], language: String, date: String, text: [String], tags: [String]) {
         self.newsURL = newsURL
         self.title = title
+        self.briefExplanation = briefExplanation
         self.author = author
         self.language = language
         self.date = date
         self.text = text
         self.tags = tags
     }
-    
+
+    func convertToDict() -> Any {
+        return ["newsURL": newsURL, "title": title,"briefExplanation": briefExplanation,"author": author,"language": language,"date": date,"text": text,"tags": tags]
+    }
 }
