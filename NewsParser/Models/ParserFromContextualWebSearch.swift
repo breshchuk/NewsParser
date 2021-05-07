594fafa62b6ccd4a64c4812c79a83a9091129cec
Dmitriy Breshchuk <breschuk1@gmail.com>
2021-05-07 12:18:48 +0300

    add ParserFromContextualWebSearch class(with protocol NewsParser) and add realization of getNews function

---
+++ NewsParser/Models/ParserFromContextualWebSearch.swift
@@ -0,0 +1,67 @@
+//
+//  ParserFromContextualWebSearch.swift
+//  NewsParser
+//
+//  Created by dimam on 1.05.21.
+//
+
+import Foundation
+
+class ParserFromContextualWebSearch: NewsParserProtocol {
+    
+   private let headers = [
+        "x-rapidapi-key": "6ec37195abmshf37dbe9410e2c75p1cb6e5jsnb229f1e80064",
+        "x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com"
+    ]
+    
+   private var requestURL = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/TrendingNewsAPI?pageNumber=1&pageSize=10&withThumbnails=false&location=us"
+
+   lazy private var request: NSMutableURLRequest = {
+        let request = NSMutableURLRequest(
+            url: NSURL(string: requestURL)! as URL,
+            cachePolicy: .useProtocolCachePolicy,
+            timeoutInterval: 10.0
+        )
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        return request
+    }()
+    
+    func getNews(completionHandler: @escaping (_ result: Result<[NewsProtocol], ParserErrors>) -> Void )   {
+
+        let session = URLSession.shared
+        let dataTask = session.dataTask (with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                completionHandler(.failure(.custom(errorStr: error.debugDescription)))
+                return
+            }
+            if let httpResponse = response as? HTTPURLResponse, (200...300).contains(httpResponse.statusCode) {
+                
+                guard let jsonDict = try? JSONSerialization.jsonObject(with: data!, options: []) as? [String : Any] else {
+                    completionHandler(.failure(.custom(errorStr: "Cannot parse to jsonObject")))
+                    return
+                }
+                guard let jsonValues = try? JSONSerialization.data(
+                        withJSONObject: jsonDict["value"],
+                        options:[]
+                ) else {
+                    completionHandler(.failure(.custom(errorStr: "Cannot parse to jsonObject")))
+                    return
+                }
+                
+                guard let newsArr = try? JSONDecoder().decode([NewsFromContextualWebSearch].self, from: jsonValues) else {
+                    completionHandler(.failure(.custom(errorStr: "Cannot parse to jsonObject")))
+                    return
+                }
+                newsArr.forEach {
+                    $0.initAuthorsAndText()
+                }
+                completionHandler(.success(newsArr))
+            }
+        })
+
+        dataTask.resume()
+    }
+    
+    
+}
