12d36e5af0e531662cfc4dbcbda7b70bbfca6edd
Dmitriy Breshchuk <breschuk1@gmail.com>
2021-05-07 12:11:43 +0300

    add ConvertToDictFunction and change saveNews function

--- NewsParser/Models/SaveToFirebase.swift
+++ NewsParser/Models/SaveToFirebase.swift
@@ -18,9 +18,27 @@ class SaveToFirebase {
         return hash.map { String(format: "%02hhx", $0) }.joined()
     }
     
-    func saveNews(news: News) {
+    func saveNews(news: NewsProtocol) {
         let md5 = createMD5forNews(newsURL: news.newsURL)
         let newsRef = self.ref.child(md5)
-        newsRef.setValue(news.convertToDict())
+        newsRef.setValue(convertToDict(news: news))
+    }
+    
+    func convertToDict(news: NewsProtocol) -> Any {
+        var mainReturn: [String: Any] = ["newsURL": news.newsURL, "title": news.title, "briefExplanation": news.briefExplanation ,"author": news.author, "language": news.language, "date": news.date, "text": news.text, "tags": news.tags]
+        if let news = news as? News {
+            if let imagesURL = news.imagesURL, !imagesURL.isEmpty, !imagesURL.first!.isEmpty {
+                mainReturn["imageURL"] = imagesURL
+            }
+            if let textUnderTitleImage = news.textUnderTitleImage , !textUnderTitleImage.isEmpty {
+                mainReturn["textUnderTitleImage"] = textUnderTitleImage
+            }
+            if let titleImageURL = news.titleImageURL, !titleImageURL.isEmpty {
+                mainReturn["titleImageURL"] = titleImageURL
+            }
+        } else if let news = news as? NewsFromContextualWebSearch {
+            
+        }
+        return mainReturn
     }
 }
