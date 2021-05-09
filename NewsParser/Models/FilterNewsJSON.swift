0925559d9520c28c9e9d29253eff9864ce1a00d5
Dmitriy Breshchuk <breschuk1@gmail.com>
2021-05-09 19:10:31 +0300

    Rewrite and add filters

--- NewsParser/Models/FilterNewsJSON.swift
+++ NewsParser/Models/FilterNewsJSON.swift
@@ -12,44 +12,110 @@ enum FilterError: Error {
 }
 
 enum FilterParametres {
-    case author
-    case title
-    case date
-    case everything
+    case everything(author: String, title: String, fromDate: String, toDate: String)
 }
 
 protocol FilterJSON {
-    func filter(by filterParametres: [FilterParametres],jsonData: Data, completion: @escaping (Result<Data, FilterError>) -> Void )
+    func filter(
+        by filterParametres: [FilterParametres],
+        jsonData: Data,
+        completion: @escaping (Result<Data, FilterError>) -> Void
+    )
 }
 
 class FilterNewsJSON: FilterJSON {
+    
+    var JSONDict = [String: Any]()
     
     func filter(
         by filterParametres: [FilterParametres],
         jsonData: Data,
         completion: @escaping (Result<Data, FilterError>) -> Void
     ) {
-        var filteredJSON = [String: Any]()
         guard
             let jsonDict = try? JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] else {
             completion(.failure(.dataToJson))
             return
         }
-        jsonDict.forEach { key, value in
-            guard let news = value as? [String: Any] else {
-                return
-            }
-            if let authors = news["author"] as? [String] {
-                if authors.contains("Remy Tumin") {
-                    filteredJSON[key] = value
-                }
+        JSONDict = jsonDict
+        var filteredJSON = [String: Any]()
+        filterParametres.forEach {
+            switch $0 {
+            case .everything(let author, let title, let from, let to):
+                filteredJSON = filterEverything(
+                    author: author,
+                    title: title,
+                    from: from,
+                    to: to
+                )
             }
         }
-        print(filteredJSON.keys)
         guard let jsonData = try? JSONSerialization.data(withJSONObject: filteredJSON, options: []) else {
             return
         }
         completion(.success(jsonData))
+    }
+    
+    
+    private func filterByAuthor(authorName: String) -> [String: Any] {
+        var filteredJSON = [String: Any]()
+        self.JSONDict.forEach { key, value in
+            guard let news = value as? [String: Any] else {
+                return
+            }
+            if let authors = news["author"] as? [String] {
+                authors.forEach {
+                    if $0.contains(authorName) {
+                        filteredJSON[key] = value
+                    }
+                }
+            }
+        }
+        self.JSONDict = filteredJSON
+        return filteredJSON
+    }
+    
+    private func filterByTitle(title: String) -> [String: Any] {
+        var filteredJSON = [String: Any]()
+        self.JSONDict.forEach { key, value in
+            guard let news = value as? [String: Any] else {
+                return
+            }
+            if let titleFromJSON = news["title"] as? String {
+                if titleFromJSON.contains(title) {
+                    filteredJSON[key] = value
+                }
+            }
+        }
+        self.JSONDict = filteredJSON
+        return filteredJSON
+    }
+    
+    private func filterByDate(from: String,to: String) -> [String: Any] {
+        var filteredJSON = [String: Any]()
+        self.JSONDict.forEach { key, value in
+            guard let news = value as? [String: Any] else {
+                return
+            }
+            if let authors = news["title"] as? [String] {
+                if authors.contains(from) {
+                    filteredJSON[key] = value
+                }
+            }
+        }
+        self.JSONDict = filteredJSON
+        return filteredJSON
+    }
+    
+    private func filterEverything(author: String, title: String, from: String, to: String) -> [String: Any] {
+        var filteredArray = [String: Any]()
+        if !author.isEmpty {
+            filteredArray = filterByAuthor(authorName: author)
+        }
+        if !title.isEmpty {
+            filteredArray = filterByTitle(title: title)
+        }
+        return filteredArray
     }
     
 }
