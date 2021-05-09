92643371b353499833a4532e3d52baf968a6fff4
Dmitriy Breshchuk <breschuk1@gmail.com>
2021-05-09 19:11:22 +0300

    Rewrite and add properties for read & write & filter data

--- NewsParser/Controllers/FilterViewController.swift
+++ NewsParser/Controllers/FilterViewController.swift
@@ -8,7 +8,7 @@
 import Cocoa
 
 class FilterViewController: NSViewController {
-
+    
     @IBOutlet weak var filterByTitleTextFiled: NSTextField!
     
     @IBOutlet weak var filterByAuthorTextField: NSTextField!
@@ -16,22 +16,49 @@ class FilterViewController: NSViewController {
     @IBOutlet weak var fromDatePicker: NSDatePicker!
     @IBOutlet weak var toDatePicker: NSDatePicker!
     
+    private var readJSONManager: ReadData!
+    private var filterJSONManager: FilterJSON!
+    private var saveToFileManager: SaveManager!
+    private var dataForFilter: Data!
     
-    
-    
- 
-    @IBAction func filterByTitleButtonPressed(_ sender: NSButton) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        readJSONManager = ReadFromFirebase()
+        filterJSONManager = FilterNewsJSON()
+        saveToFileManager = SaveToFile()
+        
+        getDataForFilter()
         
     }
     
-    @IBAction func filterByAuthorButtonPressed(_ sender: NSButton) {
-        
+    private func getDataForFilter() {
+        readJSONManager.getData { result in
+            switch result {
+            case .success(let data):
+                self.dataForFilter = data
+            case .failure(let error):
+                self.view.presentError(error)
+            }
+        }
     }
-    
-    @IBAction func filterByDatePressed(_ sender: NSButton) {
-        
-    }
+
+    // TODO: Make date filter
     @IBAction func filterByAllPressed(_ sender: NSButton) {
-        
+                filterJSONManager.filter(
+                    by: [.everything(
+                            author: filterByAuthorTextField.stringValue,
+                            title: filterByTitleTextFiled.stringValue,
+                            fromDate: fromDatePicker.dateValue.description,
+                            toDate: toDatePicker.dateValue.description)
+                    ],
+                    jsonData: self.dataForFilter) { result in
+                    switch result {
+                    case .success(let saveData):
+                        self.saveToFileManager.save(data: saveData)
+                    case .failure(let error):
+                        self.view.presentError(error)
+                    }
+                }
+        }
     }
-}
